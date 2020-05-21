eb48084b272ea6e1c1bcbd05a927e772f34e8807
mosm <airman515@gmail.com>
2020-05-21 17:10:12 +0900

    Lower memory consumption with splitting text & merging arrays

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,12 +1,7 @@
 use suffine::IndexBuilder;
 
 fn main() {
-    let mut index_builder = IndexBuilder::new();
-    index_builder.add("I scream, you scream, we all scream for ice cream!");
-    index_builder.add("She sells seashells by the seashore.");
-
-    let index = index_builder.build();
-    for (doc_id, pos) in index.search("cream") {
-        println!("{}: {}", doc_id, pos)
-    }
+    let text = "I scream, you scream, we all scream for ice cream!";
+    let index_builder = IndexBuilder::new(text);
+    let _index = index_builder.build_in_memory();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,110 +1,244 @@
-use serde::{Deserialize, Serialize};
+use byteorder::{NativeEndian, WriteBytesExt};
+use memmap::Mmap;
+use std::borrow::Cow;
+use std::cmp::{Ordering, Reverse};
+use std::collections::BinaryHeap;
+use std::io::{self, BufWriter};
 use suffix::SuffixTable;
 
-mod suffix_table_serde {
-    use serde::{Deserialize, Deserializer, Serialize, Serializer};
-    use std::borrow::Cow;
-    use suffix::SuffixTable;
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
 
-    #[derive(Serialize, Deserialize)]
-    struct Helper<'s, 't> {
-        text: Cow<'s, str>,
-        table: Cow<'t, [u32]>,
+pub struct IndexBuilder<'s> {
+    text: &'s str,
+    block_size: u32,
+}
+
+pub struct Index<'s, 't> {
+    #[allow(dead_code)]
+    text: &'s str,
+    #[allow(dead_code)]
+    suffix_array: Cow<'t, [u32]>,
+}
+
+impl<'s, 't> Index<'s, 't> {
+    pub fn from_parts<S>(text: &'s str, suffix_array: S) -> Result<Index<'s, 't>>
+    where
+        S: Into<Cow<'t, [u32]>>,
+    {
+        Ok(Index {
+            text,
+            suffix_array: suffix_array.into(),
+        })
     }
 
-    pub fn serialize<S>(suffix_table: &SuffixTable, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        Helper {
-            text: Cow::Borrowed(suffix_table.text()),
-            table: Cow::Borrowed(suffix_table.table()),
-        }
-        .serialize(serializer)
-    }
-
-    pub fn deserialize<'de, 's, 't, D>(deserializer: D) -> Result<SuffixTable<'s, 't>, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        let s = Helper::deserialize(deserializer)?;
-        Ok(SuffixTable::from_parts(s.text, s.table))
+    pub fn from_bytes(text: &'s str, bytes: &'t [u8]) -> Result<Index<'s, 't>> {
+        Index::from_parts(text, unsafe { slice_from_bytes(bytes) })
     }
 }
 
-#[derive(Serialize, Deserialize)]
-pub struct Index {
-    doc_offsets: Vec<u32>,
-
-    #[serde(with = "suffix_table_serde")]
-    suffix_table: SuffixTable<'static, 'static>,
-}
-
-impl Index {
-    pub fn search(&self, query: &str) -> Vec<(usize, usize)> {
-        self.suffix_table
-            .positions(&query)
-            .iter()
-            .map(|p| {
-                let doc_id = match self.doc_offsets.binary_search(p) {
-                    Ok(x) => x,
-                    Err(x) => x - 1,
-                };
-                let pos_in_doc = p - self.doc_offsets[doc_id];
-                (doc_id as usize, pos_in_doc as usize)
-            })
-            .collect()
-    }
-
-    pub fn document(&self, doc_id: usize) -> Option<&str> {
-        if doc_id < self.doc_offsets.len() {
-            let begin = self.doc_offsets[doc_id] as usize;
-            let end = if doc_id == self.doc_offsets.len() - 1 {
-                self.suffix_table.text().len() - 1
-            } else {
-                (self.doc_offsets[doc_id + 1] - 1) as usize
-            };
-            Some(&self.suffix_table.text()[begin..end])
-        } else {
-            None
+impl<'s> IndexBuilder<'s> {
+    pub fn new(text: &'s str) -> IndexBuilder<'s> {
+        IndexBuilder {
+            text,
+            block_size: u32::MAX,
         }
     }
-}
 
-pub struct IndexBuilder {
-    concatenated_str: String,
-    doc_offsets: Vec<u32>,
-}
-
-impl IndexBuilder {
-    pub fn new() -> Self {
-        Default::default()
-    }
-
-    pub fn add(&mut self, text: &str) -> &mut Self {
-        self.doc_offsets.push(self.concatenated_str.len() as u32);
-
-        self.concatenated_str.push_str(&text);
-        self.concatenated_str.push('\0');
-
+    pub fn block_size(&mut self, block_size: u32) -> &mut Self {
+        self.block_size = block_size.max(1);
         self
     }
 
-    pub fn build(self) -> Index {
-        Index {
-            doc_offsets: self.doc_offsets,
-            suffix_table: SuffixTable::new(self.concatenated_str),
+    pub fn build_to_writer<W: std::io::Write>(&self, mut writer: W) -> Result<()> {
+        match self.text.len() {
+            0 => return Ok(()),
+            1 => {
+                writer.write_u32::<NativeEndian>(0)?;
+                return Ok(());
+            }
+            _ => (),
         }
+
+        if self.text.len() <= self.block_size as usize {
+            build_suffix_array_to_writer(self.text, self.text.len(), writer)?;
+        } else {
+            let heap = sort_blocks(self.text, self.block_size)?;
+            merge_blocks_to_writer(heap, writer)?;
+        }
+
+        Ok(())
+    }
+
+    pub fn build_in_memory(&self) -> Result<Index<'s, 'static>> {
+        let sa = build_suffix_array_in_memory(self.text);
+        Index::from_parts(self.text, Cow::Owned(sa))
     }
 }
 
-impl Default for IndexBuilder {
-    fn default() -> Self {
-        Self {
-            concatenated_str: "".to_string(),
-            doc_offsets: Vec::new(),
+fn build_suffix_array_in_memory(text: &str) -> Vec<u32> {
+    match text.len() {
+        0 => return Vec::new(),
+        1 => return vec![0],
+
+        _ => (),
+    }
+
+    SuffixTable::new(text)
+        .table()
+        .iter()
+        .filter(|&&x| text.is_char_boundary(x as usize))
+        .copied()
+        .collect()
+}
+
+fn build_suffix_array_to_writer<W: io::Write>(text: &str, len: usize, mut writer: W) -> Result<()> {
+    let st = SuffixTable::new(text);
+    let sa = st
+        .table()
+        .iter()
+        .filter(|&&x| (x as usize) < len && text.is_char_boundary(x as usize));
+    for x in sa {
+        writer.write_u32::<NativeEndian>(*x)?;
+    }
+
+    Ok(())
+}
+
+struct Block<'a> {
+    text: &'a str,
+    mmap: Mmap,
+    begin: usize,
+    cursor: usize,
+}
+
+impl PartialEq for Block<'_> {
+    fn eq(&self, _: &Self) -> bool {
+        unimplemented!()
+    }
+}
+
+impl Eq for Block<'_> {}
+
+impl Ord for Block<'_> {
+    fn cmp(&self, _: &Self) -> Ordering {
+        unimplemented!()
+    }
+}
+
+impl PartialOrd for Block<'_> {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(
+            self.front_suffix()
+                .as_bytes()
+                .cmp(other.front_suffix().as_bytes()),
+        )
+    }
+}
+
+impl Block<'_> {
+    fn suffix_array(&self) -> &[u32] {
+        unsafe { slice_from_bytes(&self.mmap) }
+    }
+
+    fn front_index(&self) -> usize {
+        self.suffix_array()[self.cursor] as usize
+    }
+
+    fn front_suffix(&self) -> &str {
+        &self.text[self.front_index()..]
+    }
+
+    fn next(&mut self) -> bool {
+        self.cursor += 1;
+        self.cursor < self.suffix_array().len()
+    }
+}
+
+fn calc_tail_len(text: &str, pat: &str) -> Option<usize> {
+    let mut occ_pos = 0;
+    let mut prefix_len = 1;
+    while !pat.is_char_boundary(prefix_len) {
+        prefix_len += 1;
+    }
+    while let Some(i) = &text[occ_pos..].find(&pat[..prefix_len]) {
+        if prefix_len == pat.len() {
+            return None;
+        }
+        occ_pos += *i;
+        prefix_len += 1;
+        while !pat.is_char_boundary(prefix_len) {
+            prefix_len += 1;
         }
     }
+    Some(prefix_len)
+}
+
+fn sort_blocks(text: &str, block_size: u32) -> Result<BinaryHeap<Reverse<Block>>> {
+    let mut heap = BinaryHeap::new();
+
+    let mut begin = 0;
+    while begin < text.len() {
+        let end = {
+            let mut i = (begin + block_size as usize).min(text.len());
+            while !text.is_char_boundary(i) {
+                i += 1;
+            }
+            i
+        };
+        let (end, end_with_tail) = if end == text.len() {
+            (end, end)
+        } else {
+            match calc_tail_len(&text[begin..end], &text[end..]) {
+                Some(l) => (end, end + l),
+                None => (text.len(), text.len()),
+            }
+        };
+
+        let file = tempfile::tempfile()?;
+        {
+            let writer = BufWriter::new(&file);
+            build_suffix_array_to_writer(&text[begin..end_with_tail], end - begin, writer)?;
+        }
+
+        let block = Block {
+            text: &text[begin..],
+            mmap: unsafe { Mmap::map(&file)? },
+            begin,
+            cursor: 0,
+        };
+        heap.push(Reverse(block));
+
+        begin = end;
+    }
+
+    Ok(heap)
+}
+
+fn merge_blocks_to_writer<W: io::Write>(
+    mut heap: BinaryHeap<Reverse<Block>>,
+    mut writer: W,
+) -> Result<()> {
+    while let Some(Reverse(mut block)) = heap.pop() {
+        let idx = (block.front_index() + block.begin) as u32;
+        writer.write_u32::<NativeEndian>(idx)?;
+
+        if block.next() {
+            heap.push(Reverse(block));
+        }
+    }
+
+    Ok(())
+}
+
+unsafe fn slice_from_bytes<T>(bytes: &[u8]) -> &[T] {
+    assert_eq!(0, std::mem::size_of::<T>() % std::mem::size_of::<u8>());
+    assert_eq!(0, bytes.len() % std::mem::size_of::<T>());
+
+    let ratio = std::mem::size_of::<T>() / std::mem::size_of::<u8>();
+    let ptr = bytes.as_ptr() as *const T;
+    let length = bytes.len() / ratio;
+
+    std::slice::from_raw_parts(ptr, length)
 }
 
 #[cfg(test)]
@@ -113,62 +247,49 @@ extern crate quickcheck_macros;
 
 #[cfg(test)]
 mod tests {
-    use crate::IndexBuilder;
+    use crate::{Index, IndexBuilder};
+    use itertools::Itertools;
 
-    #[quickcheck]
-    fn single_document(text: String) {
-        let mut index_builder = IndexBuilder::new();
-        index_builder.add(&text);
+    fn check_suffix_array(text: &str, suffix_array: &[u32]) {
+        let actual: Vec<u32> = itertools::sorted(suffix_array.iter()).map(|x| *x).collect();
+        let expected: Vec<u32> = (0..text.len())
+            .filter(|&x| text.is_char_boundary(x as usize))
+            .map(|x| x as u32)
+            .collect();
+        assert_eq!(actual, expected);
 
-        let index = index_builder.build();
-        assert_eq!(index.search(""), vec![]);
-        if !text.is_empty() {
-            assert_eq!(index.search(&text), vec![(0, 0)]);
-        }
-        assert_eq!(index.document(0).unwrap(), text);
+        let sorted = suffix_array
+            .iter()
+            .tuple_windows()
+            .all(|(a, b)| &text[*a as usize..] < &text[*b as usize..]);
+        assert!(sorted);
     }
 
     #[quickcheck]
-    fn multiple_documents(texts: Vec<String>) {
-        let mut index_builder = IndexBuilder::new();
-        for text in &texts {
-            if text.contains('\0') {
-                return; // TODO
-            }
-            index_builder.add(&text);
-        }
-
-        let index = index_builder.build();
-        assert_eq!(index.search(""), vec![]);
-        for text in &texts {
-            for (doc_id, pos) in index.search(&text) {
-                let doc_text = index.document(doc_id).unwrap();
-                assert_eq!(&doc_text[pos..pos + text.len()], text);
-            }
-        }
+    fn build_in_memory(text: String) {
+        let index = IndexBuilder::new(&text).build_in_memory().unwrap();
+        check_suffix_array(&text, &index.suffix_array);
     }
 
-    #[test]
-    fn nonexistence() {
-        let mut index_builder = IndexBuilder::new();
-        index_builder.add("a");
-        index_builder.add("b");
-
-        let index = index_builder.build();
-        assert_eq!(index.search("c"), vec![]);
-        assert_eq!(index.document(5), None);
+    #[quickcheck]
+    fn build_to_writer_with_blocks(text: String, block_size: u32) {
+        let mut buf = Vec::new();
+        IndexBuilder::new(&text)
+            .block_size(block_size)
+            .build_to_writer(&mut buf)
+            .unwrap();
+        let index = Index::from_bytes(&text, &buf).unwrap();
+        check_suffix_array(&text, &index.suffix_array);
     }
 
-    #[test]
-    fn duplication() {
-        let mut index_builder = IndexBuilder::new();
-        index_builder.add("a");
-        index_builder.add("a");
-        index_builder.add("ab");
-
-        let index = index_builder.build();
-        let mut result = index.search("a");
-        result.sort_unstable();
-        assert_eq!(result, vec![(0, 0), (1, 0), (2, 0)]);
+    #[quickcheck]
+    fn build_to_writer_without_blocks(text: String) {
+        let mut buf = Vec::new();
+        IndexBuilder::new(&text)
+            .block_size(u32::MAX)
+            .build_to_writer(&mut buf)
+            .unwrap();
+        let index = Index::from_bytes(&text, &buf).unwrap();
+        check_suffix_array(&text, &index.suffix_array);
     }
 }
